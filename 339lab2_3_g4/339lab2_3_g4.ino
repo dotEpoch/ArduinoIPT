97a8f7cad22821293a7582a4fcb73b528e5d773d
Vincent Aucoin <vincent.aucoin@mail.mcgill.ca>
2025-01-27 14:28:36 -0500

    Fixed counting button press

--- 339lab2_3_g4/339lab2_3_g4.ino
+++ 339lab2_3_g4/339lab2_3_g4.ino
@@ -1,30 +1,65 @@
 bool newData = false;
-
+unsigned long startMillis;
+unsigned long time;
 char receivedChar;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  //startMillis = millis(); // initial start time
 
   //Pins
-  pinMode(6, INPUT);
-  pinMode(A5, INPUT);
+  pinMode(2, INPUT);
+  analogWrite(A5, INPUT);
+
+  //Interrupts
+  attachInterrupt(digitalPinToInterrupt(2), loop, HIGH);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  static int count = 0;
-  static bool pressed = false;
-  static int time = 0;
   //buttonPress();
   
-  if (digitalRead(6) == HIGH) {
+  static int count = 0;
+  static bool pressed = false;
+  
+  if (digitalRead(2) == HIGH) {
     if (pressed == false) {
       pressed = true;
 
       count++;
-      float voltage = range(analogRead(A5), 0, 1023, 0, 5.0);
+      float voltage = map(analogRead(A5), 0, 1023, 0, 5.0);
+      time = millis();
+
+      Serial.print(count);
+      Serial.print(",");
+      Serial.print(voltage);
+      //Serial.print(analogRead(A5));
+      Serial.print(",");
+      Serial.print(time);
+      Serial.println(",");
+
+    }
+  }
+  else {
+    pressed = false;
+
+  }
+  delay(1000);
+  //Serial.print("Number of button presses: \t");
+  //Serial.println(count);
+
+}
+
+/*void buttonPress(){
+
+  if (digitalRead(2) == HIGH) {
+    if (pressed == false) {
+      pressed = true;
+
+      count++;
+      float voltage = map(analogRead(A5), 0, 1023, 0, 5.0);
+      time = millis();
 
       Serial.print(count);
       Serial.print(",");
@@ -32,23 +67,11 @@ void loop() {
       Serial.print(",");
       Serial.print(time);
       Serial.print(",");
-      
-
-
+      Serial.println();
 
     }
-
-    //while (digitalRead(2) == HIGH) delay(10);
   }
   else {
     pressed = false;
   }
-
-  delay(1000)
-  time++;
-
-}
-
-//void buttonPress(){
-
-//}
+}*/
