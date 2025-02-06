fb4788ec175e45ed3e420b513ae81fad55c356d7
Vincent Aucoin <vincent.aucoin@mail.mcgill.ca>
2025-02-06 13:16:07 -0500

    Coded time interval python script

---
+++ Lab3/Code/IPC_geiger_helper.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 12:51:37 2025
+
+@author: vaucoi
+"""
+
+import serial
+import time
+import os
+from datetime import datetime
+
+### Functions ###
+
+def main():
+    # ----------- Init ------------- #
+    ser = serial.Serial(port = 'COM5', baudrate = 115200)
+    time.sleep(3)
+    # ----------- Clear ------------ #
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+    time.sleep(1)
+    
+    # ------------ File -------------#
+    
+        # Add a check if file exists
+        
+    #if os.path.isfile("ButtonPress.txt"): 
+     #   print("File already exists. Overwrite?")
+      #  print("--> Yes haha")
+       # text_file = open("ButtonPressreal.txt", "w")
+    #else:
+        
+    date_format = datetime.now().strftime("%Y-%m-%d-%Hh%Mm%Ss")
+    
+    text_file = open("P:\ArduinoIPT\Lab3\Data\lab3_data_IPC_200clicks_(178_5mm)_sample1__{0}.txt".format(date_format), "w")
+    data_points = []
+    #end_time = time.time() + 200 # Record data for 2 minutes 20s
+    
+    # ------------ Comms ------------#
+    while(len(data_points) <= 200):
+        ser.flush()
+        #time.sleep(0.1)
+    
+        
+        if (ser.in_waiting > 0):
+    
+            # Text file
+            time_interval_raw = ser.read_until(','.encode()).decode('utf-8')
+            print(time_interval_raw)
+            text_file.write(time_interval_raw)
+    
+        
+            # Histogram
+            time_interval = int(time_interval_raw[:-1])
+            data_points.append(time_interval)
+            
+            #stdout
+            print("_____________________Data point_______________________")
+            print("\t Number of time elapsed since last click:", time_interval, "ms") if type(time_interval) is int else print("[COUNT ERROR]")
+            
+
+    print("Number of data points:", len(data_points))
+    
+    # --------- Close ---------#
+    text_file.close()
+    ser.close()
+
+    return data_points
+
+
+
+### Script ###
+
+if __name__ == '__main__':
+    try:
+        main()
+    except: 
+        os.system("ser.close()")
+        main()
+    print("port opened")
