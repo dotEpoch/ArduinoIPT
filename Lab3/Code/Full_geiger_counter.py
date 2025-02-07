f7704e91b8f6b3021b435dee020304779a061e6a
Vincent Aucoin <vincent.aucoin@mail.mcgill.ca>
2025-02-07 14:35:11 -0500

    Huge IPC datasets for MEDIUM and HIGH means

--- Lab3/Code/Full_geiger_counter.py
+++ Lab3/Code/Full_geiger_counter.py
@@ -6,18 +6,19 @@ Created on Thu Feb  6 13:19:07 2025
 """
 import serial
 import time
-import IPC_geiger_helper
-import CPI_geiger_helper
+#import IPC_geiger_helper
+#import CPI_geiger_helper
+import test_helper
 
 
 # # ----------- Init ------------- #
-ser = serial.Serial(port = 'COM5', baudrate = 115200)
+#ser = serial.Serial(port = 'COM5', baudrate = 115200)
 time.sleep(3)
 print("port open")
 
 # # ----------- Clear ------------ #
-ser.reset_input_buffer()
-ser.reset_output_buffer()
+#ser.reset_input_buffer()
+#ser.reset_output_buffer()
 time.sleep(1)
 
 
@@ -26,9 +27,11 @@ time.sleep(1)
 #Get user input -> IPC or CPI
 def start_collection(points, dist, mean):
     try: 
-        mode = input("Input data collection mode (CPI or IPC): ")
+        mode = "I" #input("Input data collection mode (CPI or IPC): ")
         
-        if (mode == "CPI"):
+        if (mode == "I"):
+            x = test_helper.main("meow")
+            print(x)
             #ser.write('c'.encode())
             time.sleep(1)
             #return CPI_geiger_helper
@@ -47,9 +50,9 @@ def make_hist():
 
 def get_data():
     #-------- User Parameters ------------#
-    num_points = input("Number of data points: ")
-    distance = input("Distance of radioactive source: ") #for naming purposes
-    mean = input("Mean category (HIGH, MID, LOWER): ") #for naming purposes
+    num_points = 1 #input("Number of data points: ")
+    distance =1 #input("Distance of radioactive source: ") #for naming purposes
+    mean =1 #input("Mean category (HIGH, MID, LOWER): ") #for naming purposes
     data_array = start_collection(num_points, distance, mean)
     
     # --------- Plot ---------#
@@ -62,7 +65,7 @@ def get_data():
 
 ### Script ###
 
-sample_number = input("Number of samples to collect: ")
+sample_number = 1 #input("Number of samples to collect: ")
 
 for i in range(sample_number):
     data_array = get_data()
@@ -73,7 +76,7 @@ for i in range(sample_number):
 
 
 # --------- Close ---------#
-ser.close()
+#ser.close()
 print("done")
 
 
--- Lab3/Code/IPC_geiger_helper.py
+++ Lab3/Code/IPC_geiger_helper.py
@@ -25,11 +25,11 @@ def main():
     
     date_format = datetime.now().strftime("%Y-%m-%d-%Hh%Mm%Ss")
     
-    text_file = open("P:\ArduinoIPT\Lab3\Data\lab3_data_IPC_200clicks_(130mm)_sample1__{0}.txt".format(date_format), "w")
+    text_file = open("P:\ArduinoIPT\Lab3\Data\lab3_data_IPC_10000clicks_(130mm)_sampleBIG__{0}.txt".format(date_format), "w")
     data_points = []
     
     # ------------ Comms ------------#
-    while(len(data_points) < 200):
+    while(len(data_points) < 10000):
         ser.flush()
         
         if (ser.in_waiting > 0):
